ae09b22e942dae1a37d511c0e7f76d1c146dcd74
Oscar Santana <oscarsantana2014@gmail.com>
2021-08-23 14:27:52 -0500

    adding table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,4 +1,20 @@
 -- create database codeup_test_db;
 USE codeup_test_db;
-show tables ;
+CREATE TABLE IF NOT EXISTS albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50) DEFAULT 'NONE',
+    name         VARCHAR(100) NOT NULL,
+    release_date  YEAR         NOT NULL,
+    sales        int UNSIGNED NOT NULL,
+    genre        VARCHAR(100),
+    PRIMARY KEY
+        (
+         id
+            )
+);
+
+
+show tables;
 DESCRIBE albums;
+
---
+++ albums_seeder.sql
@@ -0,0 +1,14 @@
+use codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, genre,sales)
+VALUES ('Michael Jackson','Thriller',1982,'pop',70.0),
+('AC/DC','Back In Black',1980,'rock',50.0),
+('MEAT LOAF','Bat Out Of Hell',1977,'rock',50.0),
+('Whitney Houston','The Bodyguard',1992,'r&b',45.0),
+('Pink Floyd','The Dark Side Of The Moon',1973,'rock',45.0),
+('Eagles','Their Greatest Hits',1976,'rock',42.0);
+
+
+
+
+show tables ;
