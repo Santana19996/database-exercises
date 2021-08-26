9ff578874aa8b8ab7ec79188d391c36e00e55085
Oscar Santana <oscarsantana2014@gmail.com>
2021-08-26 09:34:31 -0500

    Finished exercises

---
+++ aliases_exercises.sql
@@ -0,0 +1,11 @@
+use employees;
+
+SELECT CONCAT (last_name,' ',first_name) as full_name
+FROM employees;
+
+
+SELECT Concat (emp_no,' -',last_name, ' ',first_name) as full_name,
+       birth_date as DOB from employees
+LIMIT 10;
+
+
